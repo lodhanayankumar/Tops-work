3fd22329b946fc281b2e2b5883a508eb008df3f3
Nayankumar <lodha.nayankumar@gmail.com>
2026-03-22 19:20:49 +0530

    Delete assignment/module/module1

--- assignment/module/module1/lab_task/1.ass_se_1_lab.1.cpp
+++
@@ -1,8 +0,0 @@
-//Write a simple "Hello World" program in two different programming languages of
-//your choice. Compare the structure and syntax.
-#include<stdio.h>
-int main()
-{
-	printf(" Hello World ");
-}
-
