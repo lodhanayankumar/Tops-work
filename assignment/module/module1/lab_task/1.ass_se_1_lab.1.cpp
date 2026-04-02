6f6261b8b15a89f3dd86644bf04e0733c98b31b1
Nayankumar <lodha.nayankumar@gmail.com>
2026-04-02 13:31:00 +0530

    Add files via upload

---
+++ assignment/module/module1/lab_task/1.ass_se_1_lab.1.cpp
@@ -0,0 +1,8 @@
+//Write a simple "Hello World" program in two different programming languages of
+//your choice. Compare the structure and syntax.
+#include<stdio.h>
+int main()
+{
+	printf(" Hello World ");
+}
+
