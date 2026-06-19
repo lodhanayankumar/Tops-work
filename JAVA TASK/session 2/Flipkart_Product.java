6fbe6d8010bd29460320f29c503024765228df2c
Nayankumar <lodha.nayankumar@gmail.com>
2026-06-19 19:55:34 +0530

    Add files via upload

---
+++ JAVA TASK/session 2/Flipkart_Product.java
@@ -0,0 +1,17 @@
+
+public class Flipkart_Product {
+
+	public static void main(String[] args) {
+		//product detail
+       String productName = "groundnut seeds";
+       double price = 90.95;
+       boolean inStock = true;
+       float rating = 4.2f;
+       //display product
+       System.out.println("product name : " + productName);
+       System.out.println("price : " + price);
+       System.out.println("instock : " + inStock);
+       System.out.println("Rating : " + rating);
+	}
+
+}
