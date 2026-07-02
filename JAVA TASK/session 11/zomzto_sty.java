6742d3dcca85a1d27c0a9bb4b966c5cbfe38a2f4
Nayankumar <lodha.nayankumar@gmail.com>
2026-07-02 07:55:50 +0530

    Add files via upload

---
+++ JAVA TASK/session 11/zomzto_sty.java
@@ -0,0 +1,13 @@
+/*5.Explain in your own words how abstraction in Java (using abstract classes or interfaces) acts 
+ * like an API contract between the service layer and database layer in a Zomato-style food ordering app. 
+ * Give one example for each (abstract class and interface) showing what methods would be exposed.
+ */
+
+public class zomzto_sty {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
