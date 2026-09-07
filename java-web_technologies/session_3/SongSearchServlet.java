0495cd09453add80ee7e1196079f42ab06792a7c
Nayankumar <lodha.nayankumar@gmail.com>
2026-09-07 09:47:12 +0530

    Refactor SongSearchServlet to simplify output

--- java-web_technologies/session_3/SongSearchServlet.java
+++ java-web_technologies/session_3/SongSearchServlet.java
@@ -12,17 +12,12 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/SearchServlet")
 public class SongSearchServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)  throws ServletException, IOException {
 
         String song = request.getParameter("songName");
         String artist = request.getParameter("artist");
-
-        response.setContentType("text/html");
-
+        
         PrintWriter out = response.getWriter();
-
-        out.println("<h2>Searching for " + song + " by " + artist + "</h2>");
+        out.println("Searching for " + song + " by " + artist );
     }
 }
