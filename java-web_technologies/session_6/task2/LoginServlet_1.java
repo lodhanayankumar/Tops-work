015052f3e22d533ad8a1b8f678dff5f9a30323f9
Nayankumar <lodha.nayankumar@gmail.com>
2026-09-09 17:18:26 +0530

    Add files via upload

---
+++ java-web_technologies/session_6/task2/LoginServlet_1.java
@@ -0,0 +1,65 @@
+package test13;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/login_2")
+public class LoginServlet_1 extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if (username.equals("nayan") && password.equals("1234")) {
+
+            HttpSession session = request.getSession();
+            session.setAttribute("username", username);
+            response.sendRedirect("Deshboard.jsp");
+        } else {
+            response.getWriter().println("Invalid Username or Password");
+        }
+    }
+}
+
+/*login2.html
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <form action="login_2" method="post">
+        Username:<input type="text" name="username"><br>
+        Password:<input type="password" name="password"><br>
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>*/
+
+/*Deshboard.jsp
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+pageEncoding="UTF-8"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Dashboard</title>
+</head>
+<body>
+<%
+    String username = (String) session.getAttribute("username");
+%>
+<h1>Welcome <%= username %></h1>
+<p>You have successfully logged in.</p>
+</body>
+</html>*/
